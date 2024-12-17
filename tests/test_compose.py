e4db19c5c138a9dbee085fb65c697dce9e66cecb
Julien RIALLAND <julien.rialland@groupe-asten.fr>
2024-12-17 16:39:17 +0100

    test compose

---
+++ tests/test_compose.py
@@ -0,0 +1,29 @@
+from pytestcontainers import using_containers, get_free_tcp_port, wait_for_tcp_port
+import os
+
+# locate the compose file
+compose_file = os.path.join(os.path.dirname(__file__), "compose-example.yml")
+
+# get a free tcp port
+free_tcp_port = get_free_tcp_port()
+
+
+# ------------------------------------------------------------------------------
+@using_containers(compose_file=compose_file, compose_env={"PORT": str(free_tcp_port)})
+def test_check_compose_works(stack: using_containers):
+    wait_for_tcp_port(free_tcp_port)
+
+
+# ------------------------------------------------------------------------------
+@using_containers(
+    inline_compose={
+        "services": {
+            "web": {
+                "image": "nginx:latest",
+                "ports": [f"{free_tcp_port}:80"],
+            },
+        },
+    }
+)
+def test_check_inline_compose_works(stack: using_containers):
+    wait_for_tcp_port(free_tcp_port)
